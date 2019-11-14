62cd224de0c3602a0958a8ac894e211b72cb7809
nAtnAt' raspberry <natnatraspberry@nAtnAts-MacBook-Air.local>
2019-11-14 11:54:53 +0800

    Attempt at visualisation

--- scripts/tanks_combined_data.R
+++ scripts/tanks_combined_data.R
@@ -11,3 +11,13 @@ watercorp_qpcr_tidied_data
 tanks_qpcr_combined_data <- inner_join(watercorp_qpcr_tidied_data, waresh_qpcr_tidied_data)
 
 view(tanks_qpcr_combined_data)
+
+
+## Visualisation 
+
+tanks_qpcr_combined_data %>% 
+  ggplot(aes(x = waresh_acanthamoeba_spp,
+           y = temperature,
+           colour = tank,
+           shape = sample_type)) +
+  geom_point()
